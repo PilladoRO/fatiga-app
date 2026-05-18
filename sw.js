da267e1f0ec144817140182aac0b2401fd7b840e
PilladoRO <jefeturno.romeral@pilladoempresas.cl>
2026-05-18 05:04:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,28 +2,6 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-
-  const request = event.request;
-
-  // ❌ NUNCA interceptar POST (CRÍTICO)
-  if (request.method !== "GET") {
-    return;
-  }
-
-  // ❌ NO interferir con Google Apps Script
-  if (request.url.includes("script.google.com")) {
-    return;
-  }
-
-  // ✔ solo cache básico para navegación
-  event.respondWith(
-    fetch(request).catch(() => {
-      return caches.match(request);
-    })
-  );
+self.addEventListener("fetch", () => {
+  // solo habilita cache básico
 });
